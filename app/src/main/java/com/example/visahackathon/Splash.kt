df15b3547da7362cd6e5cf626a15912a647297fb
Diego Coviella <covielladiego@ufl.edu>
2020-06-26 12:41:33 -0400

    added splash page

---
+++ app/src/main/java/com/example/visahackathon/Splash.kt
@@ -0,0 +1,26 @@
+package com.example.visahackathon
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class Splash : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
